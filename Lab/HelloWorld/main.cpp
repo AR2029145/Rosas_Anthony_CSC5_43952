9efedaa1dd4c8db73112f39c4a6cf10ba058c4ad
AR2029145 <Arosas2@student.rcc.edu>
2015-03-06 14:17:22 -0800

    HelloWorld

---
+++ Lab/HelloWorld/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: Anthony Rosas
+ * Purpose:First Program
+ * Created on March 6, 2015, 1:44 PM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Output Hello World
+    cout<<"Hello World"<<endl;
+    cout<<"Hello World\n";
+    //Exit stage right!
+    return 0;
+}
+
