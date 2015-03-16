07395339c5189c1fa1583a1edee117aca50a0d6b
ar2029145 <arosas@student.rcc.edu>
2015-03-16 10:54:46 -0700

    Signed-off-by: ar2029145 <arosas@student.rcc.edu>

---
+++ Hmwk/Assignment_3/CppApplication_2/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Anthony Rosas
+ *
+ * Created on March 16, 2015, 10:13 AM
+ */
+
+//system Libraries
+#include <cstdlib>//Random rand() ,sand
+#include <iostream>//Keyboard/Monitor
+#include <fstream>//File I/O
+#include <ctime>//time fuctions
+#include <cmath>// math function library
+#include<iomanip>//format lib
+using namespace std;
+//user Libraries
+//global constants
+//function prototypes
+//execution begins here
+int main(int argc, char** argv) {
+//Declare Variables
+float 
+float 
+char  
+//Calculate the monthly payment
+float
+float 
+//outpu
+
+//Close the stream
+return 0;
+}
+
