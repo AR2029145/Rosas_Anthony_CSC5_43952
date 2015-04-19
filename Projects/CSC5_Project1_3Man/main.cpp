f8e8f891c0632c96f7833a64e72027f56550a9c1
AR2029145 <Arosas2@student.rcc.edu>
2015-04-18 20:23:05 -0700

    Project bailed on started new project

--- Projects/CSC5_Project1_3Man/main.cpp
+++ Projects/CSC5_Project1_3Man/main.cpp
@@ -18,12 +18,20 @@ using namespace std;
 //user Libraries
 //global constants
 //function prototypes
- 
+void init();//(promt user for #of players and names)
+
+//'const' is constant variable, can not be modified or reassigned
+const unsigned int MAXPLAY = 8;//Max amount of players 
+
+//Declare Variables
+unsigned int playCot;//user input for player #
+char pName[21];//Players name up to 21 char(array)
+
+//array of player structures
+player players[MAXPLAY];
 
 //execution begins here
-
 //Declare structures
-
 struct player{// Players Name and points stored as a structure 
 	string name;//Players name
 	unsigned int points = 30;// Players points 
@@ -34,98 +42,32 @@ struct player{// Players Name and points stored as a structure
 
 int main(int argc, char** argv)
 {
-	//'const' is constant variable, can not be modified or reassigned
-	const unsigned int MAXPLAY = 8;//Max amount of players 
-	
-        //Declare Variables
-	unsigned int playCot;//user input for player #
-        char pName[21];//Players name up to 21 char(array)
-        
-        //array of player structures
-	player players[MAXPLAY];
-	
-
-	//example array visualized, 4 player game, missing players are NULL:
-	//
-	//[				
-	//	{ 
-	//		name: "Andy",          
-	//		points : 30,	
-	//		active = true 
-	//	},			
-	//	{ 
-	//		name: "Anthony",       
-	//		points : 30,	
-	//		active = true 
-	//	},				
-	//	{ 
-	//		name: "Tony", 
-	//		points : 30,	
-	//		active = true 
-	//	},			{ 
-	//		name: "Jaime",         
-	//		points : 30,	
-	//		active = true 
-	//	},				
-	//	{ 
-	//		name: NULL,			 
-	//		points : 30,	
-	//		active = false 
-	//	},			
-	//	{ 
-	//		name: NULL,			 
-	//		points : 30,	
-	//		active = false 
-	//	},			
-	//	{ 
-	//		name: NULL,			 
-	//		points : 30,	
-	//		active = false 
-	//	},			
-	//	{ 
-	//		name: NULL,			 
-	//		points : 30,	
-	//		active = false 
-	//	},
-	//]
-
-
+		
 	//Prompt user for input  
 	
 	
-        do
+        
+               init();    
+                
+         return 0;	
+}
+
+void init(){
+    do
         {
             cout<<"Please enter a valid amount of players."<<endl;
-            cin >> playCot;//for every player in player_count, assign them a name and make them active example: players[index].name = "Anthony"; players[index].active = true;
-    //playCot -1 b/c array is 0-7(index and we don't want/have a "8th" index )
+            cin >> playCot;//for every player in player_count, assign them a name and make them active example: players[index].name = "Anthony"; players[index].active = true; playCot -1 b/c array is 0-7(index and we don't want/have a "8th" index )
 
          }while(playCot <= 0 || playCot >= 9 );
              
-	for(int playTemp = 0; playTemp <= playCot -1 ; playTemp++){//playTemp is a temp hold for the computer to count off of
-            
-            
+	for(int playTemp = 0; playTemp <= playCot -1 ; playTemp++){//playTemp(incerment counter place holder) is used in the array (ZERO-7 (players 1-8)) as a counter to place each name in its given spot
             do
             {
                 cout<<"Please enter a valid player name."<<endl;
                 cin>>pName;
              }while(strlen(pName) <= 0);//strlen is a function call from<cstringlib>
+           players[playTemp].name = pName;//(array)[counter++1].(part of structure) = (name user inputs)
+           players[playTemp].active = true;//(array)[counter++1].(part of structure) = (verification of active player)
+           
         }
-                   
-          
-        
-        
-        
-        /*2.	2 to 8
-*Total of 7 - Player to right of roller loses a point 
-*Total of 11 - Player to left of roller loses a point
-*Total of 9 – Everybody loses a Point
-*Any 3 or sum to 3 - Three man loses a point 
-*1 and 4 – Lose a turn 
-*1 and 5 – Lose a turn 
-*Doubles – Choose a player to lose points
-*/
-         
-        
-        
-	return 0;	
 }
---
+++ Projects/Project#2/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Zedek
+ *
+ * Created on April 18, 2015, 8:16 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
